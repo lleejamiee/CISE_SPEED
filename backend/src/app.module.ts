09cf9ea1dc20965f8d3537057776a61746f5d2ab
lleejamiee <85136381+lleejamiee@users.noreply.github.com>
2024-10-07 19:50:09 +1300

    fix[development]: path changed in app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { UserModule } from 'src/api/users/user.module';
+import { UserModule } from './api/users/user.module';
 import { ArticleModule } from './api/articles/article.module';
 
 @Module({
