8af69afb6fcda0fd8122a9e9fbe543b1d4a02612
Teresa <teresamatalavea@yahoo.com>
2024-10-08 19:41:40 +1300

    fix[analysis]: resolve merge conflicts with Development

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './api/users/user.module';
 import { ArticleModule } from './api/articles/article.module';
+import { SeMethodModule } from './api/semethod/semethod.module';
 
 @Module({
   imports: [
